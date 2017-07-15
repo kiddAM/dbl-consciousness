4812454b52c9be83635186f77e877273d416b2b9
Candace Johnson <crj2121@columbia.edu>
2017-07-15 15:20:31 -0700

    added location function

--- getCoordinates.py
+++ getCoordinates.py
@@ -2,18 +2,19 @@ import googlemaps
 from datetime import datetime
 
 gmaps = googlemaps.Client(key='AIzaSyCnVeLt_qHh4zguJBK5zYF85iJvRTCi0eM')
-address = "9600 14th Bay Street"
-city = "Norfolk"
-state = "VA"
+address1 = "9600 14th Bay Street"
+city1 = "Norfolk"
+state1 = "VA"
 
 # Geocoding an address
 #geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-geocode_result = gmaps.geocode(("%s,%s,%s")%(address,city,state))
-#geocode_result = gmaps.geocode('Block 9600 14th Bay Street, Norfolk, VA')
-lat = (geocode_result[0]["geometry"]["bounds"]["northeast"]["lat"])
-lng = (geocode_result[0]["geometry"]["bounds"]["northeast"]["lng"])
+def getGeoLocation(address, city, state):
+    geocode_result = gmaps.geocode(("%s,%s,%s")%(address,city,state))
+#   geocode_result = gmaps.geocode('Block 9600 14th Bay Street, Norfolk, VA')
+    lat = (geocode_result[0]["geometry"]["bounds"]["northeast"]["lat"])
+    lng = (geocode_result[0]["geometry"]["bounds"]["northeast"]["lng"])
 
-print (lat, lng)
+    return (lat, lng)
 #for key in geocode_result[0].items():
 #	print (key) 
 #print (geocode_result[0].keys())
@@ -22,10 +23,7 @@ print (lat, lng)
 reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
 # Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
 #print (type(directions_result))
 #print ((directions_result[0]))
+
+print (getAddress(address1,city1, state1))
