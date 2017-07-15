f397ae9036b265e91c4baf76dd2f92c66aa35103
Terence Jacobs <TJ@Terences-MacBook-Air.local>
2017-07-15 17:42:46 -0400

    updated getlocations to print lat and lng and added additional requirements

--- getCoordinates.py
+++ getCoordinates.py
@@ -1,1 +1,31 @@
-'test'
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='AIzaSyCnVeLt_qHh4zguJBK5zYF85iJvRTCi0eM')
+address = "9600 14th Bay Street"
+city = "Norfolk"
+state = "VA"
+
+# Geocoding an address
+#geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+geocode_result = gmaps.geocode(("%s,%s,%s")%(address,city,state))
+#geocode_result = gmaps.geocode('Block 9600 14th Bay Street, Norfolk, VA')
+lat = (geocode_result[0]["geometry"]["bounds"]["northeast"]["lat"])
+lng = (geocode_result[0]["geometry"]["bounds"]["northeast"]["lng"])
+
+print (lat, lng)
+#for key in geocode_result[0].items():
+#	print (key) 
+#print (geocode_result[0].keys())
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+#print (type(directions_result))
+#print ((directions_result[0]))
