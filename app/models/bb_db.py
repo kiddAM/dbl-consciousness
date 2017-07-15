ecc2422c7cb4162ea0e9a316297ceb34af0ad7fd
Jib Adegunloye <jadegunloye@Jibs-MacBook-Pro.local>
2017-07-15 16:45:20 -0700

    finished making the basic blackbusiness model

--- app/models/bb_db.py
+++ app/models/bb_db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import orm
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,11 +10,15 @@ class BlackBusinessMapping(Base):
     __tablename__ = "Black_Businesses"
 
 
+
     _id = Column('id', Integer, primary_key=True)
-    name = Column('name', String)
-    owner = Column('owner_name', String)
-    founding = Column('founding', String, nullable=False)
-    address = Column('address', String)
+    name = Column('name', String, nullable=False)
+    owner = Column('owner_name', String, nullable=False)
+    service = Column('service', String, nullable=True)
+    x = Column('lat', Float)
+    y = Column('long', Float)
+    founding = Column('founding', String)
+    address = Column('address', String, nullable=False)
 
     def getid(self):
         return self._id
