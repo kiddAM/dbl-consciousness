246607c2bb3c1826956bb695fbea0944857c45be
John Anukem <john.anukem@stitichfix.com>
2017-07-16 09:45:27 -0700

    Added black_owned_biz list

--- app/controllers/web_scraper.py
+++ app/controllers/web_scraper.py
@@ -13,8 +13,13 @@ class WebScraper:
         if(source == 'http://www.blackbusinesslist.com/online-directory1'):
             resources = black_business_list_data
 
-        
         return resources 
+
+    def black_owned_biz_data(self):
+        site = 'http://www.blackownedbiz.com/directory/location/united-states/'
+        black_owned_biz_data = {}
+        tree = get_tree_from(site)
+        black_owned_biz_data = parse_black_owned_biz_data(tree)
 
     def black_business_list_data(self):
         site ='http://www.blackbusinesslist.com/online-directory1' 
@@ -51,3 +56,31 @@ def get_title_and_address(title):
         new_title = title[0:delimeter_index]
         address = title[delimeter_index + 1: len(title)]
         return (new_title, address)
+
+def parse_black_owned_biz_data(tree):
+    businesses = {}
+    titles = tree.xpath("//span[@class='listing_default']/text()")
+    addresses = tree.xpath("//div[@class='listing_results_address']/text()")
+    addresses = delete_newlines(addresses)
+    print(titles)
+    string_of_addresses = "".join(addresses) 
+    addresses = " ".join(string_of_addresses.split()).split("United States")
+    for i in range(8):
+        print(titles[i])
+        print(addresses[i])
+
+
+def delete_newlines(data):
+    
+    new_data = data
+
+    for address in new_data:
+        address = address.strip('\n').strip(" ")
+    return new_data
+
+for i in range(2,1257):
+    tree = get_tree_from("http://www.blackownedbiz.com/directory/location/united-states/" + "?page=" + str(i) )
+    parse_black_owned_biz_data(tree)
+
+
+
